8c3780281813f421c8b521577143d0c063ddd5fc
Morad12 <ennajmimorad@gmail.com>
2019-05-30 17:36:48 +0200

    test

--- Server/src/dataBaseConnection/test.java
+++ Server/src/dataBaseConnection/test.java
@@ -11,6 +11,7 @@ public class test {
 	public static void main(String[] args) {
 		try {
 			System.out.println(MySqlConnetion.getUser("lufy").toString());
+			System.out.println("hi");
 			
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
